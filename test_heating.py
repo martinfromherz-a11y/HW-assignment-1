d1d8d64b7f02a4dc85e9d3f7ed38a37e9bbc1fa6
Martin Fromherz <martin.fromherz@gmail.com>
2026-03-30 20:07:36 +0200

    pytest test

--- test_heating.py
+++ test_heating.py
@@ -2,7 +2,7 @@ from main import dh_annual_cost, gas_annual_cost
 
 def test_dh_annual_cost():
     result = dh_annual_cost(20000, 79.23, 74.82)
-    assert round(result, 2) == 1575.63
+    assert round(result, 2) == 5
 
 def test_gas_annual_cost():
     result = gas_annual_cost(20000, 60.42, 58.63, 7000, 100, 0.05, 20)
